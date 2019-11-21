316374e5363458afe9a8a939137d82a6645bd606
Liviu Preda <liviupreda@gmail.com>
2019-11-21 12:17:30 +0200

    getCard() and store card data in state.drawnCards[]

--- src/Deck.js
+++ src/Deck.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 import Card from "./Card";
 
-const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
+    this.state = { deck: null, drawnCards: [] };
+    this.getCard = this.getCard.bind(this);
   }
 
   componentDidMount() {
@@ -16,14 +17,32 @@ class Deck extends Component {
 
   async fetchAPIData() {
     // Fetch random card data from Deck;
-    let deck = await axios.get(API_URL);
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/?deck_count=1`);
     this.setState({ deck: deck.data });
+  }
+
+  async getCard() {
+    let deckID = this.state.deck.deck_id;
+    // Get card from Deck by ID
+    let cardResponse = await axios.get(`${API_BASE_URL}/${deckID}/draw/`);
+    let card = cardResponse.data.cards[0];
+    this.setState(st => ({
+      drawnCards: [
+        ...st.drawnCards,
+        {
+          id: card.code,
+          image: card.image,
+          name: `${card.value} of ${card.suit}`
+        }
+      ]
+    }));
   }
 
   render() {
     return (
       <div className="Deck">
         <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card!</button>
         <Card />
       </div>
     );
