d92f3ef2c2f7897773ef15e17ad1e499649e7c86
Liviu Preda <liviupreda@gmail.com>
2019-11-21 11:18:18 +0200

    add state to Deck; install axios; create fetchAPIData() in Deck.js

--- src/Deck.js
+++ src/Deck.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Card from "./Card";
 
+const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deck: null };
+  }
+
+  componentDidMount() {
+    this.fetchAPIData();
+  }
+
+  fetchAPIData() {
+    // Fetch random card data from Deck;
+  }
+
   render() {
     return (
       <div className="Deck">
-        <h1>Deck of Cards</h1>
+        <h1>Card Dealer</h1>
         <Card />
       </div>
     );
