d6a826d38e88dcb55470e344550e2a986110d042
Liviu Preda <liviupreda@gmail.com>
2019-11-22 09:26:42 +0200

    move random pos and angle variables in contructor

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 import "./Card.css";
 
 class Card extends Component {
-  render() {
+  constructor(props) {
+    super(props);
     // transform: translate(10px, 20px) rotate(20deg);
     // Translate and rotate each card by a random amount, so that they
     // don't stack against each other
     let angle = Math.random() * 90 - 45;
     let xTranslate = Math.random() * 40 - 20;
     let yTranslate = Math.random() * 40 - 20;
-    let transform = `translate(${xTranslate}, ${yTranslate}) rotate(${angle})`;
+    this._transform = `translate(${xTranslate}px, ${yTranslate}px) rotate(${angle}deg)`;
+  }
 
+  render() {
     return (
-      <img className="Card" src={this.props.image} alt={this.props.name} />
+      <img
+        style={{ transform: this._transform }}
+        className="Card"
+        src={this.props.image}
+        alt={this.props.name}
+      />
     );
   }
 }
